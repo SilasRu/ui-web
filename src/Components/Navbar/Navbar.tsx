b67be2a5886aa9a4889180b093432be2aae86dfc
silas.rudolf@nexoya.com <silas.rudolf@nexoya.com>
2022-08-07 09:50:42 +0200

    feat: navbar

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import './Navbar.css';
+import TranscriptDropdown from '../TranscriptDropdown/TranscriptDropdown'
+import MediationOutlinedIcon from '@mui/icons-material/MediationOutlined';
+
+const Navbar = () => {
+  const handleTranscriptImport = async (transcriptName: string) => {
+    console.log(transcriptName);
+  };
+  return (
+    <div className="navbar">
+      <div className="navbar-wrapper">
+        <div className="navbar-selector">
+            <TranscriptDropdown handleTranscriptImport={handleTranscriptImport}/>
+        </div>
+        <div className="navbar-items">
+        <MediationOutlinedIcon/>
+          <div className="navbar-item">
+            <p className='navbar-title'>Transcript Analyzer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/Views/Template.tsx
+++ src/Views/Template.tsx
@@ -4,24 +4,28 @@ import Chart from 'src/Components/SentimentChart/SentimentChart';
 import Table from 'src/Components/Table/Table';
 import SpeakerNetwork from 'src/Components/SpeakerNetwork/SpeakerNetwork';
 import Keywords from 'src/Components/Keywords/Keywords';
+import Navbar from 'src/Components/Navbar/Navbar';
 
 const Template = () => {
   return (
     <div>
-      <div className="widgets">
-        <Widget type="user" />
-        <Widget type="order" />
-        <Widget type="earning" />
-        <Widget type="balance" />
-      </div>
-      <div className="charts">
-        <SpeakerNetwork />
-        <Keywords/>
-        <Chart />
-      </div>
-      <div className="listContainer">
-        <div className="listTitle">Latest Transactions</div>
-        <Table />
+      <div className="homeContainer">
+        <Navbar/>
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <SpeakerNetwork />
+          <Keywords />
+          <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
+        </div>
       </div>
     </div>
   );
