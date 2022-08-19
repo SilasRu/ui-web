a46208e4c41886714ee0563fd7b2ee0618a46897
silas.rudolf@nexoya.com <silas.rudolf@nexoya.com>
2022-08-19 09:39:11 +0200

    feat: entities reworked

--- src/Components/Entities/EntityChip.tsx
+++ src/Components/Entities/EntityChip.tsx
@@ -1,6 +1,8 @@
 import { useTheme } from '@mui/material/styles';
 import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
 
 const colorCodes: Array<'primary' | 'success' | 'secondary' | 'error' | 'warning' | 'default' | 'info'> = ['primary', 'success', 'secondary', 'error', 'warning'];
 
@@ -8,26 +10,34 @@ const EntityChip = (props: { entityGroups: object; selectedEntities: number }) =
   const entityGroupKey = Object.keys(props.entityGroups)[props.selectedEntities];
   const entityGroupToDisplay = props.entityGroups[entityGroupKey];
 
-  const stacks = [];
-  const chunkSize = 3;
-  for (let i = 0; i < entityGroupToDisplay.length; i += chunkSize) {
-    const chunk = entityGroupToDisplay.slice(i, i + chunkSize);
-    const stack = chunk.map((nGram, index) => <Chip key={index} label={nGram.word} color={colorCodes[props.selectedEntities]} variant="outlined"></Chip>);
-
-    stacks.push(
-      <Stack direction="row" spacing={1}>
-        {stack}
-      </Stack>
-    );
-  }
+  const ListItem = styled('li')(({ theme }) => ({
+    margin: theme.spacing(0.5),
+  }));
 
   return (
     <div className="entity-chips">
-      <Stack spacing={1} alignItems="center">
-        {stacks.map((key, index) => (
-          <div key={index}>{key}</div>
-        ))}
-      </Stack>
+      <Paper
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+          listStyle: 'none',
+          overflow: 'auto',
+          p: 0.5,
+          m: 0,
+          maxHeight: '160px',
+          boxShadow: '10px 10px 10px rgba(0, 0, 0, 0);',
+        }}
+        component="ul"
+      >
+        {entityGroupToDisplay.map((data, key) => {
+          return (
+            <ListItem key={key}>
+              <Chip key={key} label={data.word} color={colorCodes[props.selectedEntities]} variant="outlined"></Chip>
+            </ListItem>
+          );
+        })}
+      </Paper>
     </div>
   );
 };
